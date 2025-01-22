805888bcf47ad1cc09c6a07b51171ea0a5f85f98
carlos.borja <carlos.borja@energiaplus.es>
2025-01-22 14:56:03 +0100

    refactor

--- clients/urls.py
+++ clients/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.list_clients, name="list_clients"),
-    # path("new/", views.agregar_client, name="add_client"),
     path("new/", views.ClientUpsertView.as_view(), name="add_client"),
     path('edit/<int:pk>/', views.ClientUpsertView.as_view(), name='edit_client'),
 
